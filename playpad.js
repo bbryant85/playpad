de673fb3f67e3878c2d200ad7c8703d8e57100a9
bbryant1185 <bbryant1185@yahoo.com>
2017-05-22 15:38:17 -0400

    just playing

--- playpad.js
+++ playpad.js
@@ -1,4 +1,11 @@
 var firstName = "Budree";
 var lastName = "Bryant";
+var fullName = firstName + " " +
+lastName;
+console.log("Your first name is " + firstName.toUpperCase());
+console.log("Your last name is" + lastName);
+console.log("Your full name is" + fullName);
 
-console.log(firstName);
+var yourName = prompt("What is your name?");
+if(yourName.toLowerCase() === firstName.toLowerCase()){alert('Hey I think i know you! You are ' + yourName );}
+else {alert('Oh nice to meet you');}
